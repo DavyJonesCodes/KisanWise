7d6c91406c27056a666adcff22616ac11e5f48e1
LacomWayne <lacomwayne@gmail.com>
2024-07-27 21:48:55 +0530

    Okay

--- backend/controllers/cropController.js
+++ backend/controllers/cropController.js
@@ -13,7 +13,7 @@ exports.getCrop = async (req, res) => {
     try {
         let cropId = req.params.id;
         const data = await cropService.getCr(cropId);
-        if(data == false){
+        if(!data){
             res.status(404).json({status: "error",message: "Crop not found"});
         }else{
             res.json(data);
@@ -27,7 +27,7 @@ exports.addCrop = async (req, res) => {
     try {
         let cropData = req.body;
         const data = await cropService.addCr(cropData);
-        res.json(data);
+        res.status(201).json(data);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -38,7 +38,7 @@ exports.updateCrop = async (req, res) => {
         let cropId = req.params.id;
         let cropData = req.body;
         const data = await cropService.updateCr(cropId, cropData);
-        if(data == false){
+        if(!data){
             res.status(404).json({status: "error",message: "Crop not found"});
         }else{
             res.json(data);
@@ -52,7 +52,7 @@ exports.deleteCrop = async (req, res) => {
     try {
         let cropId = req.params.id;
         const data = await cropService.deleteCr(cropId);
-        if(data == false){
+        if(!data){
             res.status(404).json({status: "error",message: "Crop not found"});
         }else{
             res.json({message: "Crop deleted successfully"});
