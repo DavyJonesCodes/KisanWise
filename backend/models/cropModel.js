d1abe4db5b2ed3e78d8a99d1307a37425be9752c
Dark Knight <abhinavkhatana50@gmail.com>
2024-07-27 20:31:13 +0530

    fixed crop Model

--- backend/models/cropModel.js
+++ backend/models/cropModel.js
@@ -1,6 +1,11 @@
+const mongoose = require('mongoose');
+
+// Define the schema for crops
 const cropSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    tips: { type: String, required: true },
-    imageUrl: { type: String, required: true}
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  tips: { type: String, required: true },
+  imageUrl: { type: String, required: true }
 });
+
+module.exports = mongoose.model('Crop', cropSchema);
